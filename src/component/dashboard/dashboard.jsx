4487c446c98e4d1405f78e5ff00d0a83d9b66ccd
negi-007 <sourabhn376@gmail.com>
2023-11-24 17:48:19 +0530

    changed

--- src/component/dashboard/dashboard.jsx
+++ src/component/dashboard/dashboard.jsx
@@ -3,7 +3,7 @@ const dashboard = () => {
         <div className="w-5/6 flex flex-col justify-between min-h-screen bg-white text-black gap-6">
             <div className="flex flex-row w-full justify-between gap-6">
                 <div className="flex flex-col">
-                    <p className="font-semibold">Progress of Today</p>
+                    <p className="font-bold text-xl2">Progress of Today</p>
                 </div>
 
                 <div>
