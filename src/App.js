21900b9bc042a9c18ac74f9a3a9e71898b2b3c71
zero <ahmadfirdosh008@gmail.com>
2023-11-24 15:11:47 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import './App.css';
-
 import Login from './component/Login/login';
+
 
 function App() {
   return (
     <div className="">
-      <Login/>
-      <h1 className=''>My React App</h1>
-      <p>This is my first React app.</p>
+      <Login/> 
     </div>
   );
 }
