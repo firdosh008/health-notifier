16c9dc72436fa20660850cdb94ab8f2f3805208f
negi-007 <sourabhn376@gmail.com>
2023-11-24 15:46:10 +0530

    updated sign/signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Login from './component/Login/login';
+import Signup from './component/signup/signup';
 
 
 function App() {
   return (
     <div className="">
       <Login/> 
+      <Signup/>
     </div>
   );
 }
